1561d4cb9e366879dbb474b6028ae7d4f4117870
Pierre Yager <pierre.y@gmail.com>
2025-05-06 23:34:19 +0200

    Make trait Engine "dyn Trait" compatible

--- src/engine.rs
+++ src/engine.rs
@@ -2,7 +2,7 @@ use raylib::prelude::*;
 use std::path::PathBuf;
 
 pub trait Engine {
-    fn new() -> Self;
+    fn new() -> Self where Self: Sized;
     fn initialize(&mut self, rl: &mut RaylibHandle, thread: &RaylibThread,paths: Vec<PathBuf>) -> bool;
     fn render_frame(&mut self, dt: f32, rl: &mut RaylibHandle, thread: &RaylibThread, framebuffer: &mut RenderTexture2D) -> bool;
 }
